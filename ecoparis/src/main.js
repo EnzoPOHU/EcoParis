0271503a36658a10f3031b5cf8245aef22cc244f
Enzo POHU <enzopohu91@gmail.com>
2025-04-02 23:50:07 +0200

    init projet ecoparis with vue3, vuetify and tailwindcss/vite

---
+++ ecoparis/src/main.js
@@ -0,0 +1,14 @@
+import { registerPlugins } from '@/plugins'
+
+import App from './App.vue'
+
+import { createApp } from 'vue'
+
+import './assets/tailwind.css'
+import './main.css'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
